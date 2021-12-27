36d9ded1dea28aa8aa082b18a56dc37435bf4c40
Himanshu-Pandey-04 <himanshuvpandey@gmail.com>
2021-12-27 20:42:33 +0530

    Updates

---
+++ precache-manifest.6ae7c98cfd5cb0a2cca41a43298c4328.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5cc4bbbc9b42543ac0c6",
+    "url": "/e-commerce-scooties/static/css/main.acd05a1c.chunk.css"
+  },
+  {
+    "revision": "5cc4bbbc9b42543ac0c6",
+    "url": "/e-commerce-scooties/static/js/main.5cc4bbbc.chunk.js"
+  },
+  {
+    "revision": "d5c42a8c4c207fefbe95",
+    "url": "/e-commerce-scooties/static/js/1.d5c42a8c.chunk.js"
+  },
+  {
+    "revision": "47e59267222e4646df1b",
+    "url": "/e-commerce-scooties/static/js/2.47e59267.chunk.js"
+  },
+  {
+    "revision": "e03272ac028e66cdc08f",
+    "url": "/e-commerce-scooties/static/js/runtime~main.e03272ac.js"
+  },
+  {
+    "revision": "365d713856a856a610b717f1a37accad",
+    "url": "/e-commerce-scooties/index.html"
+  }
+];
