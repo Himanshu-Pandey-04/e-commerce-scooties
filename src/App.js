9ffc23368ccd4df1b8e9a29907b46339fc35ebd9
Himanshu-Pandey-04 <himanshuvpandey@gmail.com>
2021-12-27 18:43:06 +0530

    Final Version

--- src/App.js
+++ src/App.js
@@ -1,29 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import closeSubMenu from './components/NavBar/NavBar';
 
 import NavBar from './components/NavBar/NavBar';
 
-function App() {
-  return (
-    <div className="App">
-      <NavBar/>
-      <div className='body'>
-          <div className='product'>
-              {/* <script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
-              <lottie-player src="https://assets9.lottiefiles.com/packages/lf20_z9gwyc2b.json"  background="transparent"  speed="1"  style="width: 300px; height: 300px;"  loop controls autoplay></lottie-player> */}
-              <div className='images'>
-                  <img id='circle' src='circle_anim.gif'/>
-                  <img id='prod' src='scooty.png'/>
-              </div>
-          </div>
-          <div className='info'>
-              <span className='headline'>A neat headline comes here !</span>
-              <span className='description'>A neat sample text to come here super soon. Till then admire the authentic and delightful User Interface Design of the Website. We are glad you came here, thanks for visiting.</span>
-              <button className='details'><a href='/'>Show Details</a></button>
-          </div>
-      </div>
-    </div>
-  );
-}
+import React, { Component } from 'react'
 
-export default App;
+export default class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <NavBar/>
+        <div className='body' onClick={this.closeSubMenu}>
+            <div className='product'>
+                <div className='images'>
+                    <img id='circle' src='./images/circle_anim.gif' alt='animated circle'/>
+                    <img id='prod' src='./images/scooty.png' alt='product'/>
+                </div>
+            </div>
+            <div className='info' onClick={this.closeSubMenu}>
+                <span className='headline'>A neat headline comes here !</span>
+                <span className='description'>A neat sample text to come here super soon. Till then admire the authentic and delightful User Interface Design of the Website. We are glad you came here, thanks for visiting.</span>
+                <button className='details'><a href='/'>Show Details</a></button>
+            </div>
+        </div>
+      </div>
+    );
+  }
+}
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -7,22 +7,14 @@ export default class NavBar extends Component {
         this.state = { show: false };
     }
 
-    toggleMenu = () => {
-        var property = document.getElementsByClassName('submenu')[0].style.display;
-        if(property === 'flex') property = 'none';
-        else property = 'flex';
-        
-    }
-    
-
     render() {
         return (
             <>
                 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"/>
                 <div className='header'>
                     <div className='top-header'>
-                        <a className='support'><i className='fa fa-phone'></i>Support : +91 - 9090909090</a>
-                        <a className='book-ride' href='/'><i class="fa fa-motorcycle"></i> Book a FREE Test Ride Now</a>
+                        <a className='support' href='/'><i className='fa fa-phone'></i>Support : +91 - 9090909090</a>
+                        <a className='book-ride' href='/'><i className="fa fa-motorcycle"></i> Book a FREE Test Ride Now</a>
                         <div className='options'>
                             <a href='/'>Contact</a>
                             <a href='/'>Career</a>
@@ -31,7 +23,7 @@ export default class NavBar extends Component {
                     </div>
                     <div className='toggle-menu'><i class="fa fa-bars" onClick={toggleMenu}></i></div>
                     <div className='bottom-header'>
-                        <div className='logo'><i className="fa fa-square"></i></div>
+                        <div className='logo' onClick={this.closeSubMenu}><i className="fa fa-square"></i></div>
                         <div className='options'>
                             <button className='btn btn-center' href='/'>About</button>
                             <button className='btn btn-center' href='/'>Locate Store</button>
@@ -67,7 +59,29 @@ function toggleMenu() {
 
 function toggleSubMenu() {
     var sub_menu = document.getElementsByClassName('sub-menu')[0];
-    if(sub_menu.style.display === 'none') sub_menu.style.display = 'flex';
+    if(sub_menu.style.display == 'none') sub_menu.style.display = 'flex';
     else sub_menu.style.display = 'none';
-    console.log('Reached');
+    console.log('toggleSubMenu');
 }
+
+function closeMenu() {
+    var mediaQ = window.matchMedia("(max-width: 700px)");
+    if(mediaQ.matches)
+    {
+        var menu = document.getElementsByClassName('bottom-header')[0];
+        menu.style.display = 'none';
+    }
+}
+
+function closeSubMenu() {
+    var sub_menu = document.getElementsByClassName('sub-menu')[0];
+    sub_menu.style.display = 'none';
+    console.log('closeSubMenu');
+}
+
+function closeMenus() {
+    closeMenu();
+    closeSubMenu();
+}
+
+window.onscroll = closeMenus;
